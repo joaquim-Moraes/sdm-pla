cbe953bc8a434535c10244878f2b4851de0a1c99
Joaquim Gomes de Moraes <82411680@ulife.com.br>
2025-08-22 11:13:22 -0300

    criação de variaveis e seu tipos de convercoes em JavaScript

---
+++ variaveis.js
@@ -0,0 +1,37 @@
+var nome = "Joaquim"
+const altura = 1.79
+let idade = 19
+idade = 20
+console.log("Meu nome é " + nome + ", tenho " + idade + " anos e minha altura é " + altura + "metros.")
+
+// String 
+let texto = "String"
+
+// Number 
+
+let numero = 10 
+let numero2 =  10.5
+// Boolean
+
+let verdadeiro = true
+
+let falso = false
+// null
+
+let nulo = null
+// Undefined
+
+let indefinido = undefined
+// Objeto
+
+let objeto = {nome: "Joaquim", idade: 19, altura: 1.79}
+// Array
+
+let lista = ["Joaquim", "carlos", "Ana"]
+
+// Conversão de tipos 
+
+// String para Number
+let stringParaNumero = Number("10")
+let stringParaNumero2 = parseInt("10.5")
+let stringParaNumero3 = parseFloat("10.5")
