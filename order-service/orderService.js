7d8c68f8e214d4b0f2295bbc10148254d10717a9
Joaquim Gomes de Moraes <82411680@ulife.com.br>
2025-09-19 09:47:16 -0300

    Adicionando microsservissos

---
+++ order-service/orderService.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const pedidos = [];
+
+app.post('/pedidos', (req,res) => {
+    const pedido = req.body;
+    console.log(`Pedido recebido para o usuário ID: ${pedido.userId}`);
+
+    pedidos.push(pedido);
+
+    res.send({ message: 'Pedido criado com sucesso!', pedido });
+});
+
+app.get('/pedidos', (req, res) => {
+    res.send({ pedidos });
+});
+
+app.listen(4000, () =>
+    console.log('Order Service rodando em: http://localhost:4000')
+);
